dcb31120ab959993bf8e2ce228b31773b2aecb30
Drolpik <mrbarti98@gmail.com>
2019-11-13 18:55:15 +0100

    Początkowe zatwierdzenie

---
+++ src/winsim/FXMLDocumentController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package winsim;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Konrad
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private Button btn;
+    
+    @FXML
+    private void changeClass(Event event) {
+        //System.out.println("You clicked me!");
+        //label.setText("Hello World!");
+        
+       label.setText("Clicked");
+       //btn.getStyleClass().add("pressed");
+       Button b = (Button)event.getSource();
+       b.getStyleClass().add("pressed");
+       //btn.setStyle("-fx-padding: 2 0 0 2;");
+       
+       
+    }
+    
+    @FXML
+    private void changeBack(Event event) {
+        //System.out.println("You clicked me!");
+        //label.setText("Hello World!");
+        
+       label.setText("WOW");
+       //btn.getStyleClass().remove(1);
+       //btn.getStyleClass().removeAll(new ArrayList<>("pressed"));
+       
+       Button b = (Button)event.getSource();
+       
+       b.getStyleClass().removeAll("pressed");
+       b.getStyleClass().removeAll("default");
+       
+    }
+    
+    @FXML
+    private void closeApp() {
+        Platform.exit();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
